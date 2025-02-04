e187c584ed2dd08fb3f9c494e62de5bdf77846a7
Juan <juancoutinhoflooxmongagua@gmail.com>
2025-02-04 09:49:38 -0300

    fix: express validator error

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ let app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(expressValidator());
 
 consign().include('routes').include('utils').into(app);
 
-app.listen(3000, '127.0.0.1', ()=>{
+app.listen(4000, '127.0.0.1', ()=>{
 
     console.log('servidor rodando!');
 
